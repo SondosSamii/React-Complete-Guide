d1c32b30f5e9a61194ab19d04e079e69368512a4
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2023-08-30 16:05:42 +0300

    Add paragraph to show hidden years

--- 05-React-State-Events/01-starting-setup/src/components/Expenses/Expenses.js
+++ 05-React-State-Events/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = ({ items }) => {
+  const years = [2023, 2022, 2021, 2020, 2019];
+
+  const [filteredYear, setFilteredYear] = useState(years[0]);
+  const [joinedFilteredYears, setJoinedFilteredYears] = useState(
+    years.filter((year) => year !== 2023).join(", ")
+  );
+
+  const filterYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    setJoinedFilteredYears(
+      years.filter((year) => year !== parseInt(selectedYear)).join(", ")
+    );
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        filteredYear={filteredYear}
+        years={years}
+        onChangeYearFilter={filterYearHandler}
+      />
+      <p style={{ textAlign: "center", color: "white" }}>
+        Data for these years: [{joinedFilteredYears}] is hidden
+      </p>
       {items.map((expense) => (
         <ExpenseItem key={expense.id} item={expense} />
       ))}
