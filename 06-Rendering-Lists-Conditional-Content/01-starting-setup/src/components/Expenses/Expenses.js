5a9b08762216a376cc28dcc279a470fbf5434aff
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2023-08-31 10:29:30 +0300

    Copy project of section 5 to 'Section 6'

---
+++ 06-Rendering-Lists-Conditional-Content/01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = ({ items }) => {
+  const years = [2023, 2022, 2021, 2020, 2019];
+
+  const [filteredYear, setFilteredYear] = useState(years[0]);
+
+  let joinedFilteredYears = years
+    .reverse()
+    .filter((year) => year !== parseInt(filteredYear))
+    .join(", ");
+
+  const filterYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        filteredYear={filteredYear}
+        years={years}
+        onChangeYearFilter={filterYearHandler}
+      />
+      <p style={{ textAlign: "center", color: "white" }}>
+        Data for these years: [{joinedFilteredYears}] is hidden
+      </p>
+      {items.map((expense) => (
+        <ExpenseItem key={expense.id} item={expense} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
---
+++ 06-Rendering-Lists-Conditional-Content/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = ({ onSaveExpense }) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const inputChangeHandler = (inputState, event) => {
+    inputState(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    onSaveExpense(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={(event) => inputChangeHandler(setEnteredTitle, event)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={(event) => inputChangeHandler(setEnteredAmount, event)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            value={enteredDate}
+            onChange={(event) => inputChangeHandler(setEnteredDate, event)}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
