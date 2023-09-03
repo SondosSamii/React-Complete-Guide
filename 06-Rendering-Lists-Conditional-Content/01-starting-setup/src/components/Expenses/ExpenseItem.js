9c6c32730ae2e8d28869a383923dfaf8734fb117
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2023-09-03 10:21:34 +0300

    Add li tag to every element in 'Expenses List'

--- 06-Rendering-Lists-Conditional-Content/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 06-Rendering-Lists-Conditional-Content/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,15 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = ({ item }) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={item.date} />
-      <div className="expense-item__description">
-        <h2>{item.title}</h2>
-        <div className="expense-item__price">${item.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={item.date} />
+        <div className="expense-item__description">
+          <h2>{item.title}</h2>
+          <div className="expense-item__price">${item.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
