37123c3a8510a97e18cd34ad754b6eef5b889aa0
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2024-02-27 11:07:57 +0200

    feat: add game container and the game log

--- 04-React-Essentials-Deep-Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-React-Essentials-Deep-Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import { GameContainer } from "./components/GameContainer/GameContainer";
+import Log from "./components/GameContainer/Log";
 
 function App() {
-  
+    const [gameTurns, setGameTurns] = useState([]);
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <GameContainer onPlay={setGameTurns} />
+      <Log gameTurns={gameTurns} />
+    </main>
+  );
 }
 
-export default App
+export default App;
