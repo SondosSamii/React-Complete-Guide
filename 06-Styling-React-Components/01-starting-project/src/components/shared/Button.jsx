21eb76ef4df41f062f3fed371848a1e6c27dd970
SondosSamii <sondos.sami.abuzeid@gmail.com>
2024-03-07 21:57:23 +0200

    refactor[section 6]: enhance the overall structure and organization of the Button component

--- 06-Styling-React-Components/01-starting-project/src/components/shared/Button.jsx
+++ 06-Styling-React-Components/01-starting-project/src/components/shared/Button.jsx
@@ -1,18 +1,17 @@
 export default function Button({ customClass, children, ...props }) {
-  let buttonClasses = "bg-none ";
+  let buttonClasses = "cursor-pointer border-0 focus:outline-none";
 
   if (customClass === "text-button") {
-    buttonClasses += "text-[#f0b322] hover:text-[#f0920e]";
+    buttonClasses += " bg-none text-[#f0b322] hover:text-[#f0920e]";
   } else if (customClass === "button") {
-    buttonClasses =
-      "font-semibold uppercase text-[#1f2937] bg-[#f0b322] hover:bg-[#f0920e] rounded-md py-4 px-8";
+    buttonClasses +=
+      " font-semibold uppercase text-[#1f2937] bg-[#f0b322] hover:bg-[#f0920e] rounded-md py-4 px-8";
+  } else {
+    buttonClasses += " bg-none";
   }
 
   return (
-    <button
-      className={`cursor-pointer border-0 focus:outline-none ${buttonClasses}`}
-      {...props}
-    >
+    <button className={buttonClasses} {...props}>
       {children}
     </button>
   );
