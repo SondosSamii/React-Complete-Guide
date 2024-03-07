945409940059a917aacb669f46987cd5081d0ce4
SondosSamii <sondos.sami.abuzeid@gmail.com>
2024-03-07 13:26:00 +0200

    style[section 6]: add dynamic class names for invalid inputs in AuthInputs component

--- 06-Styling-React-Components/01-starting-project/src/components/AuthInputs.jsx
+++ 06-Styling-React-Components/01-starting-project/src/components/AuthInputs.jsx
@@ -24,21 +24,19 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label${emailNotValid ? " invalid" : ""}`}>Email</label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label${emailNotValid ? " invalid" : ""}`}>Password</label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
+            className={passwordNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("password", event.target.value)}
           />
         </p>
       </div>
@@ -46,7 +44,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
